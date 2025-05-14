26740c35f61f7db64ae2a9e3f0f40827d6612ae7
bleumatter <steph.dimock@gmail.com>
2025-05-14 08:54:39 -0500

    Update NokiCodesButton.js

--- NokiCodes/NokiCodesButton.js
+++ NokiCodes/NokiCodesButton.js
@@ -1,8 +1,14 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+var coll = document.getElementsByClassName("blujournal_Button");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
