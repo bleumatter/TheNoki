6094f91b68a208d5e0a2c611672a29464d765630
bleumatter <steph.dimock@gmail.com>
2025-05-14 07:14:17 -0500

    Update ASL.js

--- ASL.js
+++ ASL.js
@@ -12,20 +12,3 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
-
-
-*//2ND BUTTONS//*
-var coll = document.getElementsByClassName("nokiwantedads_Button");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
