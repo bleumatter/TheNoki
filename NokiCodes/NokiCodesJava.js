58440fc9cc6e195c6ac5bcd3d116b50822ac71df
bleumatter <steph.dimock@gmail.com>
2025-05-24 15:07:02 -0400

    Update NokiCodesJava.js

--- NokiCodes/NokiCodesJava.js
+++ NokiCodes/NokiCodesJava.js
@@ -23,3 +23,30 @@ for (i = 0; i < profile_values.length; i++) {
   total_words += profile_values[i].innerHTML.split(' ').length;
 }
 document.getElementById('word-count').innerHTML = total_words;
+
+
+
+\\CLICK BUTTON\\
+document.querySelectorAll('.accordion button').forEach(button => {
+    button.addEventListener('click', function(e) {
+        let expanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !expanded);
+        
+        let content = document.getElementById(this.getAttribute('aria-controls'));
+        content.hidden = !content.hidden;
+        
+      content.setAttribute('aria-hidden', content.hidden ? 'true' : 'false');
+      
+        let icon = this.querySelector('.icon');
+        icon.textContent = content.hidden ? '+' : '−';
+    });
+
+    // Ovládanie klávesnicou
+    button.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter' || e.key === 'Space') {
+            e.preventDefault();
+            this.click();
+        }
+    });
+});
+
