2e3961b3adc48edd1eef77706f17df659c494534
bleumatter <steph.dimock@gmail.com>
2026-01-07 17:09:24 -0500

    Delete NokiCodes/NokiCodesJava.js

--- NokiCodes/NokiCodesJava.js
+++
@@ -1,7 +0,0 @@
-/* WORD COUNT STUFF */
-var profile_values = document.getElementsByClassName('word-count');
-var total_words = 0;
-for (i = 0; i < profile_values.length; i++) {
-  total_words += profile_values[i].innerHTML.split(' ').length;
-}
-document.getElementById('word-count').innerHTML = total_words;
